6008587afbfd41f9b049902d54f0ae6ec749bf5b
parzevaI <66918776+parzevaI@users.noreply.github.com>
2025-08-05 23:21:23 -0400

    added neovide support

--- init.lua
+++ init.lua
@@ -1,12 +1,37 @@
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
 vim.g.mapleader = " "
 
+
+
+
+if vim.g.neovide then
+    -- vim.g.neovide_background_color = 'darkgray'
+    vim.g.neovide_cursor_animation_length = 0.03
+    -- vim.g.neovide_background_color = '#0f1117'.printf('%x', float2nr(255 * vim.g.neovide_transparency_point))
+    vim.g.neovide_refresh_rate = 100
+
+
+    vim.g.neovide_floating_shadow = true
+    vim.g.neovide_floating_z_height = 10
+    vim.g.neovide_light_angle_degrees = 45
+    vim.g.neovide_light_radius = 5
+
+
+    vim.g.neovide_scroll_animation_length = 0.1
+
+
+    vim.g.neovide_cursor_animate_in_insert_mode = true
+end
+
+
+
+
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
 if not vim.uv.fs_stat(lazypath) then
-  local repo = "https://github.com/folke/lazy.nvim.git"
-  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
+    local repo = "https://github.com/folke/lazy.nvim.git"
+    vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
 end
 
 vim.opt.rtp:prepend(lazypath)
@@ -15,14 +40,14 @@ local lazy_config = require "configs.lazy"
 
 -- load plugins
 require("lazy").setup({
-  {
-    "NvChad/NvChad",
-    lazy = false,
-    branch = "v2.5",
-    import = "nvchad.plugins",
-  },
+    {
+        "NvChad/NvChad",
+        lazy = false,
+        branch = "v2.5",
+        import = "nvchad.plugins",
+    },
 
-  { import = "plugins" },
+    { import = "plugins" },
 }, lazy_config)
 
 -- load theme
@@ -33,5 +58,5 @@ require "options"
 require "autocmds"
 
 vim.schedule(function()
-  require "mappings"
+    require "mappings"
 end)
