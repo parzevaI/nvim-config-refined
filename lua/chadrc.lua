723260523a378ef829df59f55bb4a475def900e0
parzevaI <66918776+parzevaI@users.noreply.github.com>
2025-08-05 23:15:32 -0400

    added initial chadrc

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,24 +1,46 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
 
+-- themes
 M.base46 = {
-	theme = "aylin",
+    theme = "aylin",
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+    -- hl_override = {
+    -- 	Comment = { italic = true },
+    -- 	["@comment"] = { italic = true },
+    -- },
 }
 
--- M.nvdash = { load_on_startup = true }
--- M.ui = {
---       tabufline = {
---          lazyload = false
---      }
--- }
+-- ui
+M.ui = {
+    theme = "ashes",
+    -- transparency = true, -- uncomment for terminal emulators
+
+    -- tabs
+    tabufline = {
+        order = { "treeOffset", "buffers", "tabs" }
+    },
+
+    -- bar at the bottom
+    statusline = {
+        theme = "vscode_colored",
+        separator_style = "round",
+        overriden_modules = nil,
+    },
+
+    -- italic comments
+    hl_override = {
+        Comment = { italic = true },
+        ["@comment"] = { italic = true },
+    },
+}
+
+-- remove dashboard
+M.nvdash = { load_on_startup = false }
+
 
 return M
