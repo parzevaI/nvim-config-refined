f1443b550e5cf9e4425420b86874096a24b8e7f9
parzevaI <66918776+parzevaI@users.noreply.github.com>
2025-10-14 15:47:46 -0400

    fixed accidental tabbing in snippets

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -7,7 +7,7 @@ local M = {}
 
 -- themes
 M.base46 = {
-    theme = "aquarium",
+    theme = "jellybeans",
 
     -- hl_override = {
     -- 	Comment = { italic = true },
@@ -17,7 +17,7 @@ M.base46 = {
 
 -- ui
 M.ui = {
-    theme = "aquarium",
+    theme = "jellybeans",
     -- transparency = true, -- uncomment for terminal emulators
 
     -- tabs
